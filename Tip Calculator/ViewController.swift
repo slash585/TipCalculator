e8f394381f78a57b668efb9472118eb9bc31b393
Mehmet Ali Özdemir <mehmetaliozdemir@Mehmets-MacBook-Air.local>
2023-12-18 22:30:58 +0300

    init app

---
+++ Tip Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Mehmet Ali Özdemir on 18.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
